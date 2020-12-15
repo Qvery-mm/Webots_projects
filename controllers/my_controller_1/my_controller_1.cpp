7f954c105de1d19a551e2097d9f412463c188af1
aleks <aleksandr.kovalchukov@mail.ru>
2020-12-15 11:18:54 +0300

    further pid tuning

--- controllers/my_controller_1/my_controller_1.cpp
+++ controllers/my_controller_1/my_controller_1.cpp
@@ -34,22 +34,31 @@ int main(int argc, char **argv)
     ls[i]->enable(TIME_STEP/2);
   }
   srand(time(0));
-  double random_err_arr[4], sensors_with_random[4];
+  double random_err_arr[4], sensors_with_random[4], motor_err[4];
   double sum_err = 0.0, err = 0.0, last_err = 0.0, PID_err = 0.0;
+  for(int i = 0; i < 4; i++)
+    motor_err[i] = (double)(rand())/RAND_MAX * 0.5 - 0.25;
   
-  // double K_p = 0.034, K_i = 0.0005, K_d = 5.5; 
-  double K_p = 0.06, K_i = 0, K_d = 0;
-  double T = 1.0 / 5.0;
+  // p = 0.06
+  double K_p = 0.03, K_i = 0, K_d = 0;
+  // double T = 1.0 / 5.0;
+  // K_p = K_p * 0.6;
+  // K_i = 2.0 * K_p / T  * (TIME_STEP / 1000.0);
+  // K_d = K_p * T / 8.0  / (TIME_STEP / 1000.0);
+  // K_i /= 7;
+  // K_d *= 1.25;
+  
+  double T = 9.25 / (76.0 / 2.0);
   K_p = K_p * 0.6;
-  K_i = 2.0 * K_p / T  * (0.05);
-  K_d = K_p * T / 8.0  / (0.05);
-  // K_i /= 100;
-  // K_d *= 2;
+  K_i = 2.0 * K_p / T  * (TIME_STEP / 1000.0);
+  K_d = K_p * T / 8.0  / (TIME_STEP / 1000.0);
+  K_i /= 15;
+
   
   
   std::cout << K_p << ' ' <<K_i << ' ' << K_d << '\n';
  
-  int counter = 0;
+  // int counter = 0;
   
   while (robot->step(TIME_STEP) != -1)
   {
@@ -58,29 +67,29 @@ int main(int argc, char **argv)
       for (int i = 0; i < 4; i++) 
       {
         random_err_arr[i] = (double)(rand())/RAND_MAX * 100 - 50;
-        sensors_with_random[i] = ls[i]->getValue() + 0.1 * random_err_arr[i];
+        sensors_with_random[i] = ls[i]->getValue() + random_err_arr[i];
       }
       
       if (sensors_with_random[0] < sensors_with_random[1])
       {
-        wheels[0]->setVelocity(-5.0);
-        wheels[2]->setVelocity(-5.0);
-        wheels[1]->setVelocity(5.0);
-        wheels[3]->setVelocity(5.0);
+        wheels[0]->setVelocity(-(5.0 + motor_err[0]));
+        wheels[2]->setVelocity(-(5.0 + motor_err[2]));
+        wheels[1]->setVelocity((5.0 + motor_err[1]));
+        wheels[3]->setVelocity((5.0 + motor_err[3]));
         continue;
       }
       err = sensors_with_random[2] - sensors_with_random[3];
       sum_err += err;
       PID_err = K_p * err + K_i * sum_err + K_d * (last_err - err);
-      wheels[0]->setVelocity(5.0 - PID_err);
-      wheels[2]->setVelocity(5.0 - PID_err);
-      wheels[1]->setVelocity(5.0 + PID_err);
-      wheels[3]->setVelocity(5.0 + PID_err);
+      wheels[0]->setVelocity(5.0 - PID_err + motor_err[0]);
+      wheels[2]->setVelocity(5.0 - PID_err + motor_err[2]);
+      wheels[1]->setVelocity(5.0 + PID_err + motor_err[1]);
+      wheels[3]->setVelocity(5.0 + PID_err + motor_err[3]);
       
       // if(last_err * err < 0)
         // counter++;
       // std::cout << counter << '\n';
-      last_err = err;
+      // last_err = err;
 
   } 
   delete robot;
